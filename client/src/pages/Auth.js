990a0f1b466b2e83dc6702a86a9c8a51268ce4bb
NEKOMXTX <85887220+NEKOMXTX@users.noreply.github.com>
2024-04-10 13:57:44 +0300

    +better form from bs5 docs

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,40 +1,47 @@
 import React from "react";
-import {Container, Form} from "react-bootstrap";
-import {Card, Button, Row} from "react-bootstrap";
+import { Container, Form } from "react-bootstrap";
+import { Card, Button} from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import { REGISTRATION_ROUTE } from "../utils/consts";
 
 const Auth = () => {
     return (
-        <Container 
+        <Container
             className="d-flex justify-content-center align-items-center"
-            style={{height: window.innerHeight - 54}}
+            style={{ height: window.innerHeight - 54 }}
         >
-            <Card style={{width: 600}} className="p-5">
+            <Card style={{ width: 600 }} className="p-5">
                 <h2 className="m-auto">Авторизация</h2>
-                <Form className="d-flex flex-column">
-                    <Form.Control
-                        className="mt-3"
-                        placeholder="Введите ваш email..."
-                    />
-                    <Form.Control
-                        className="mt-3"
-                        placeholder="Введите ваш пароль..."
-                    />
-                    <Row className="justify-content-between">
+                <Form>
+                    <Form.Group className="mt-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Введите ваш email..." />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className="mt-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Введите ваш пароль..." />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+                    <div className="d-flex justify-content-between mt-3 px-3">
+
                         <div>
-                            Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегстрируйтесь!</NavLink>
+                            Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйся!</NavLink>
                         </div>
-                        <Button
-                            variant={"outline-success"}
-                        >
+
+                        <Button variant="primary" type="submit">
                             Войти
                         </Button>
-                    </Row>
-                    
+                    </div>
+
                 </Form>
             </Card>
-            
+
         </Container>
     );
 };
