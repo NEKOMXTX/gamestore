4711e4b58523f1319fefedbec19b482f6ee491ab
NEKOMXTX <85887220+NEKOMXTX@users.noreply.github.com>
2024-04-10 14:04:34 +0300

    +acceptable auth form

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Form } from "react-bootstrap";
-import { Card, Button} from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import { REGISTRATION_ROUTE } from "../utils/consts";
 
@@ -14,29 +14,22 @@ const Auth = () => {
                 <h2 className="m-auto">Авторизация</h2>
                 <Form>
                     <Form.Group className="mt-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
                         <Form.Control type="email" placeholder="Введите ваш email..." />
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                        </Form.Text>
                     </Form.Group>
 
                     <Form.Group className="mt-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
                         <Form.Control type="password" placeholder="Введите ваш пароль..." />
                     </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
-                    </Form.Group>
-                    <div className="d-flex justify-content-between mt-3 px-3">
 
-                        <div>
+                    <div className="d-flex justify-content-between mt-3">
+                        <Form.Text>
                             Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйся!</NavLink>
-                        </div>
+                        </Form.Text>
 
                         <Button variant="primary" type="submit">
                             Войти
                         </Button>
+
                     </div>
 
                 </Form>
