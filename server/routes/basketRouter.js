31535fc688cef133fdbc51602ea5cfeb6e636603
NEKOMXTX <85887220+NEKOMXTX@users.noreply.github.com>
2024-04-30 13:48:28 +0300

    +model for keys. add props in product store in client

--- server/routes/basketRouter.js
+++ server/routes/basketRouter.js
@@ -1,10 +1,9 @@
 const Router = require('express')
 const router = new Router()
 const basketController = require('../controllers/basketController')
-const checkRole = require('../middleware/checkRoleMiddleware')
 
 // ------- CRUD корзины ------- //
-router.post('/', checkRole('ADMIN'), basketController.addToBasket)
+router.post('/', basketController.addToBasket)
 router.get('/', basketController.getBasketUser)
 
 
